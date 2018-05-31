ced133d0ad80c4149b6503eadaee834b8695a6fa
quasarbright <mthundershock@gmail.com>
2018-05-31 08:39:05 -0400

    add let to local variables in functions

--- template/script.js
+++ template/script.js
@@ -18,14 +18,14 @@ function draw() {
 }
 
 function toCoord(v) {
-    x = map(v.x, 0, width, xmin, xmax)
-    y = map(v.y, height, 0, ymin, ymax)
+    let x = map(v.x, 0, width, xmin, xmax)
+    let y = map(v.y, height, 0, ymin, ymax)
     return createVector(x, y)
 }
 
 function toPixel(v) {
-    x = map(v.x, xmin, xmax, 0, width)
-    y = map(v.y, ymin, ymax, height, 0)
+    let x = map(v.x, xmin, xmax, 0, width)
+    let y = map(v.y, ymin, ymax, height, 0)
     return createVector(x, y)
 }
 
@@ -44,24 +44,24 @@ function graph(fstr) {
     }
 
     //axes
-    a = createVector(xmin, 0)
-    b = createVector(xmax, 0)
-    c = createVector(0, ymin)
-    d = createVector(0, ymax)
-    ap = toPixel(a)
-    bp = toPixel(b)
-    cp = toPixel(c)
-    dp = toPixel(d)
+    let a = createVector(xmin, 0)
+    let b = createVector(xmax, 0)
+    let c = createVector(0, ymin)
+    let d = createVector(0, ymax)
+    let ap = toPixel(a)
+    let bp = toPixel(b)
+    let cp = toPixel(c)
+    let dp = toPixel(d)
     line(ap.x, ap.y, bp.x, bp.y)
     line(cp.x, cp.y, dp.x, dp.y)
 
     //plot
     let dx = (xmax - xmin) / res
     for (let x = xmin; x < xmax; x += dx) {
-        v1Coord = createVector(x, f(x))
-        v2Coord = createVector(x + dx, f(x + dx))
-        v1Pixel = toPixel(v1Coord)
-        v2Pixel = toPixel(v2Coord)
+        let v1Coord = createVector(x, f(x))
+        let v2Coord = createVector(x + dx, f(x + dx))
+        let v1Pixel = toPixel(v1Coord)
+        let v2Pixel = toPixel(v2Coord)
         line(v1Pixel.x, v1Pixel.y, v2Pixel.x, v2Pixel.y)
     }
 }
